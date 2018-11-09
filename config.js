ec981ef75fbcc72463174711bf29509953dadac5
zhengyuaing <wang_zhengya@163.com>
2018-11-09 12:49:00 +0800

    port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.port = process.env.PORT || 3333;
+exports.port = process.env.PORT || 8088;
 exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/sdaqdoa'
 };

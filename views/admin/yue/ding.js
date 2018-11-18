c8dd38e4485cd724a16598dc8df9d6cf8e08cfdc
zhengyuaing <wang_zhengya@163.com>
2018-11-18 15:32:52 +0800

    修复一个重要bug

--- views/admin/yue/ding.js
+++ views/admin/yue/ding.js
@@ -16,7 +16,10 @@ exports.noti = function(req, res, next){
             var usernames = "";
             console.log(readers);
             readers.forEach(reader => {
-                usernames += reader.id + ",";
+                if(!reader.isFinished){
+                    usernames += reader.id + ",";
+                }
+                
             });
             console.log("拼接后的用户"+usernames);
             Ding.ding(usernames,result.general,"http://sdaoa.com:8088/account/yue/detail/"+req.params.id);
